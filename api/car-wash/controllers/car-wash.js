1263ccc215c5236fc33f3a5054f0b3feec8384cd
RusinTomasz <tomaszrusin90@gmail.com>
2021-04-02 09:33:58 +0200

    getVoivodeships and getCitiesByTypeAndVoivodeships

--- api/car-wash/controllers/car-wash.js
+++ api/car-wash/controllers/car-wash.js
@@ -1,8 +1,41 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  getVoivodeships: async (ctx) => {
+    const voivodeships = await strapi
+      .query("car-wash")
+      .model.query((qb) => {
+        qb.select("voivodeship_slug AS slug")
+          .distinct("voivodeship AS name")
+          .orderBy("slug", "ASC");
+      })
+      .fetchAll();
+
+    ctx.send(voivodeships);
+  },
+
+  getCitiesByTypeAndVoivodeship: async (ctx) => {
+    const { voivodeship, type } = ctx.params;
+
+    //TODO: make it dynamic
+    let typeId = type === "autospa" ? 2 : 1;
+
+    const cities = await strapi
+      .query("car-wash")
+      .model.query((qb) => {
+        qb.select("city_slug AS slug")
+          .distinct("city AS name")
+          .where("voivodeship_slug", voivodeship)
+          .andWhere("car_wash_type", typeId)
+          .orderBy("slug", "ASC");
+      })
+      .fetchAll();
+
+    ctx.send(cities);
+  },
+};
